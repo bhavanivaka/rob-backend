f1a9bf8c906ca3b4ff3b82da9d23720bc7d074a0
Faruq Shaik <faruq1256@gmail.com>
2017-07-17 19:00:15 +0530

    Outlets page issues resolved

--- app/outlets/outlets.js
+++ app/outlets/outlets.js
@@ -11,20 +11,28 @@ angular.module('myApp.outlets', ['ngRoute', 'firebase'])
 }])
 
 // Home controller
-.controller('OutletsCtrl', function($scope, $location, $firebaseObject ,$firebaseArray) { 
+.controller('OutletsCtrl', function($scope, $location, $firebaseObject ,$firebaseArray, $filter) { 
+	
+	$scope.date = new Date();
+	$scope.formattedDate =  $filter('date')($scope.date, "dd-MM-yyyy");
     $scope.outlet = {
         "address" : "",
         "cashback" : 0,
+		"mobile":"",
         "description" : "",
         "face" : "",
         "id" : 0,
         "locality" : "",
         "name" : "",
         "offer" : "",
+		"date":$scope.formattedDate,
         "trending" : false
     }
+	var localitiesRef = firebase.database().ref().child('localities'); 
+    $scope.localities = $firebaseArray(localitiesRef);
     var outletsRef = firebase.database().ref().child('stores'); 
-     $scope.outlets = $firebaseArray(outletsRef);
+	
+	 $scope.outlets = $firebaseArray(outletsRef);
 
      $scope.goToCashback = function(){
         $location.path('/cashbacks');
@@ -41,12 +49,13 @@ angular.module('myApp.outlets', ['ngRoute', 'firebase'])
     $scope.submit = function(form){
         if(form.$valid) {
             var key = firebase.database().ref().child('stores').push().key
-            //console.log("WORKING")
             var newOutletRef = firebase.database().ref().child('stores/'+ key)
             newOutletRef.set($scope.outlet);
             newOutletRef.update({
                 id: key
             });
+			$('#outletForm').trigger("reset");
+			$(".modal").modal("toggle");
         }
         else{
             console.log('ERROR');
